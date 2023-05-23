cbed2e03dd710d01ba0290aae230f4a4db0072e0
xy <innerfirexy@gmail.com>
2023-05-23 09:51:50 -0700

    add debug macro and code

--- lect2/hanoi/hanoi-nr.c
+++ lect2/hanoi/hanoi-nr.c
@@ -6,22 +6,26 @@ typedef struct {
 #define call(...) ({ *(++top) = (Frame) { .pc = 0, __VA_ARGS__ }; })
 #define ret()     ({ top--; })
 #define goto(loc) ({ f->pc = (loc) - 1; })
+#define offset() ((int)(top - stk)/sizeof(Frame))
+#define offset2() ((int)(top - stk))
 
 void hanoi(int n, char from, char to, char via) {
   Frame stk[64], *top = stk - 1;
 
-  printf("Initial top->pc: %d\n", top->pc);
-  printf("Initial top->n: %d\n", top->n);
+  printf("Initial stk=%p\n", stk);
+  printf("Initial stk-1=%p\n", stk-1);
+  printf("Initial &top=%p\n", top);
+  //printf("Initial offset=%lu\n", offset());
+  printf("Initial offset=%d\n", offset2());
+  printf("Initial top->pc=%d, top->n=%d\n", top->pc, top->n);
 
   call(n, from, to, via);
 
-  printf("After first call, top->pc: %d\n", top->pc);
-  printf("After first call top->n: %d\n", top->n);
+  printf("After first call, top->pc= %d, top->n=%d, offset=%d\n", top->pc, top->n, offset2());
 
   int counter = 0;
   for (Frame *f; (f = top) >= stk; f->pc++) {
     n = f->n; from = f->from; to = f->to; via = f->via;
-    printf("step %d, f->pc=%d, f->n=%d\n", counter, f->pc, f->n);
     switch (f->pc) {
       case 0: if (n == 1) { printf("%c -> %c\n", from, to); goto(4); } break;
       case 1: call(n - 1, from, via, to);   break;
@@ -30,6 +34,7 @@ void hanoi(int n, char from, char to, char via) {
       case 4: ret();                        break;
       default: assert(0);
     }
+    printf("After step %d, f->pc=%d, f->n=%d, offset=%d\n", counter, f->pc, f->n, offset2());
     counter++;
   }
 }
