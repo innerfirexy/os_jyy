daef1bb51e0bf08910a19a38878025724bca2697
xy <innerfirexy@gmail.com>
2023-06-18 10:45:38 +0800

    add hanoi-nr chatgpt version

---
+++ lect2/hanoi/hanoi-nr-chatgpt.cc
@@ -0,0 +1,23 @@
+void hanoi_none_recursive(int n, char from, char to, char via) {
+    struct Element {
+	int n; char from; char to; char via;
+    };
+
+    std::stack<Element> elements;
+    elements.push({n, from, to, via});
+
+    while (!elements.empty()) {
+	auto element = elements.top();
+	elements.pop();
+	n = element.n;
+	from = element.from;
+	to = element.to;
+	via = element.via;
+
+	if (n==1) {
+	    printf("%c -> %c\n", from, to);
+	} else {
+	    //
+	}
+    }
+}
